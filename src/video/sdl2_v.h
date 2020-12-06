81c7001d296eb7380a086bcc05a60d0ffad4a8e5
Patric Stout <truebrain@openttd.org>
2020-12-06 20:38:34 +0100

    Codechange: unroll the SDL2 main loop
    This commit prepares for the next commit, as Emscripten needs to
    have a way to trigger a single iteration of the main loop. To
    keep the real changes more clear, this commit only unrolls the
    loop, and makes no changes to the logic itself.

--- src/video/sdl2_v.h
+++ src/video/sdl2_v.h
@@ -42,12 +42,20 @@ public:
 	const char *GetName() const override { return "sdl"; }
 private:
 	int PollEvent();
+	void LoopOnce();
 	bool CreateMainSurface(uint w, uint h, bool resize);
 
 	/**
 	 * This is true to indicate that keyboard input is in text input mode, and SDL_TEXTINPUT events are enabled.
 	 */
 	bool edit_box_focused;
+
+	uint32 cur_ticks;
+	uint32 last_cur_ticks;
+	uint32 next_tick;
+
+	std::thread draw_thread;
+	std::unique_lock<std::recursive_mutex> draw_lock;
 };
 
 /** Factory for the SDL video driver. */
