4f89abd3f621192919c4f7b1d3398ebdf82f0daa
Michael Lutz <michi@icosahedron.de>
2021-01-16 16:43:25 +0100

    Add: [OpenGL] Support for 8bpp blitters.

--- src/video/win32_v.cpp
+++ src/video/win32_v.cpp
@@ -1435,7 +1435,7 @@ static FVideoDriver_Win32OpenGL iFVideoDriver_Win32OpenGL;
 
 const char *VideoDriver_Win32OpenGL::Start(const StringList &param)
 {
-	if (BlitterFactory::GetCurrentBlitter()->GetScreenDepth() != 32) return "Only 32bpp blitters supported";
+	if (BlitterFactory::GetCurrentBlitter()->GetScreenDepth() == 0) return "Only real blitters supported";
 
 	Dimension old_res = _cur_resolution; // Save current screen resolution in case of errors, as MakeWindow invalidates it.
 	this->vsync = GetDriverParamBool(param, "vsync");
@@ -1589,6 +1589,9 @@ void VideoDriver_Win32OpenGL::Paint(HWND hWnd, bool in_sizemove)
 				break;
 
 			case Blitter::PALETTE_ANIMATION_VIDEO_BACKEND:
+				OpenGLBackend::Get()->UpdatePalette(_local_palette.palette, _local_palette.first_dirty, _local_palette.count_dirty);
+				break;
+
 			case Blitter::PALETTE_ANIMATION_NONE:
 				break;
 
