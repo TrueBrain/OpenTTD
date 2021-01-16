adf46afbeaa6077df697629a6ffffb323d76b1a3
Michael Lutz <michi@icosahedron.de>
2021-01-16 16:43:08 +0100

    Codechange: [OpenGL] Only update the dirty parts of the video buffer texture.

--- src/video/win32_v.cpp
+++ src/video/win32_v.cpp
@@ -16,6 +16,8 @@
 #include "../network/network.h"
 #include "../core/math_func.hpp"
 #include "../core/random_func.hpp"
+#include "../core/mem_func.hpp"
+#include "../core/geometry_func.hpp"
 #include "../texteff.hpp"
 #include "../thread.h"
 #include "../progress.h"
@@ -1426,6 +1428,14 @@ const char *VideoDriver_Win32OpenGL::AllocateContext()
 	return OpenGLBackend::Create(&GetOGLProcAddressCallback);
 }
 
+void VideoDriver_Win32OpenGL::MakeDirty(int left, int top, int width, int height)
+{
+	this->VideoDriver_Win32Base::MakeDirty(left, top, width, height);
+
+	Rect r = {left, top, left + width, top + height};
+	this->dirty_rect = BoundingRect(this->dirty_rect, r);
+}
+
 bool VideoDriver_Win32OpenGL::ToggleFullscreen(bool full_screen)
 {
 	this->DestroyContext();
@@ -1451,6 +1461,7 @@ bool VideoDriver_Win32OpenGL::AllocateBackingStore(int w, int h, bool force)
 
 	if (this->gl_rc == nullptr) return false;
 
+	MemSetT(&this->dirty_rect, 0);
 	bool res = OpenGLBackend::Get()->Resize(w, h);
 	_wnd.buffer_bits = OpenGLBackend::Get()->GetVideoBuffer();
 	return res;
@@ -1483,8 +1494,9 @@ void VideoDriver_Win32OpenGL::Paint(HWND hWnd, bool in_sizemove)
 	GetUpdateRect(hWnd, &r, FALSE);
 	ValidateRect(hWnd, &r);
 
-	OpenGLBackend::Get()->Paint();
+	OpenGLBackend::Get()->Paint(this->dirty_rect);
 	SwapBuffers(this->dc);
+	MemSetT(&this->dirty_rect, 0);
 }
 
 #endif /* WITH_OPENGL */
