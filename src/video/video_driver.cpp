04c39e5266f5b314ceb7dfbb7fda4ba8b733e866
Patric Stout <truebrain@openttd.org>
2021-02-24 15:22:23 +0100

    Add: [Video] move GameLoop into its own thread
    This allows drawing to happen while the GameLoop is doing an
    iteration too.
    
    Sadly, not much drawing currently can be done with the GameLoop
    is running, as for example PollEvent() or UpdateWindows() can
    influence the game-state. As such, they first need to acquire a
    lock on the game-state before they can be called.
    
    Currently, the main advantage is the time spend in Paint(), which
    for non-OpenGL drivers can be a few milliseconds. For OpenGL this
    is more like 0.05 milliseconds; in these instances this change
    doesn't add any benefits for now.
    
    This is an alternative to the former "draw-thread", which moved
    the drawing in a thread for some OSes. It has similar performance
    gain as that did, although this implementation allows for more
    finer control over what suffers when the GameLoop takes too
    long: drawing or the next GameLoop. For now they both suffer
    equally.

--- src/video/video_driver.cpp
+++ src/video/video_driver.cpp
@@ -9,79 +9,135 @@
 
 #include "../stdafx.h"
 #include "../debug.h"
-#include "../core/random_func.hpp"
 #include "../gfx_func.h"
 #include "../progress.h"
 #include "../thread.h"
 #include "../window_func.h"
 #include "video_driver.hpp"
 
-void VideoDriver::Tick()
+void VideoDriver::GameLoop()
 {
-	auto cur_ticks = std::chrono::steady_clock::now();
+	this->next_game_tick += this->GetGameInterval();
 
-	/* If more than a millisecond has passed, increase the _realtime_tick. */
-	if (cur_ticks - this->last_realtime_tick > std::chrono::milliseconds(1)) {
-		auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(cur_ticks - this->last_realtime_tick);
-		_realtime_tick += delta.count();
-		this->last_realtime_tick += delta;
+	/* Avoid next_game_tick getting behind more and more if it cannot keep up. */
+	auto now = std::chrono::steady_clock::now();
+	if (this->next_game_tick < now - ALLOWED_DRIFT * this->GetGameInterval()) this->next_game_tick = now;
 
-		/* Keep the interactive randomizer a bit more random by requesting
-		 * new values when-ever we can. */
-		InteractiveRandom();
+	{
+		std::lock_guard<std::mutex> lock(this->game_state_mutex);
+
+		::GameLoop();
+	}
+}
+
+void VideoDriver::GameThread()
+{
+	while (!_exit_game) {
+		this->GameLoop();
+
+		auto now = std::chrono::steady_clock::now();
+		if (this->next_game_tick > now) {
+			std::this_thread::sleep_for(this->next_game_tick - now);
+		} else {
+			/* Either GameLoop() takes longer than GameInterval, or we are
+			 * fast-forwarding. We have to check if the draw-thread wants to
+			 * gain access to the game-state, and give him a chance to get the
+			 * lock, as otherwise we would be keeping the lock constantly.
+			 * Without the sleep_for(), we would normally not be yielding.
+			 * So, we check if the draw-thread wants to have the game-state
+			 * lock, and if so give it a chance to get it, by waiting for the
+			 * draw-thread to inform us it got the lock.
+			 */
+			if (this->request_game_state_mutex) {
+				std::mutex wait_mutex;
+				std::lock_guard<std::mutex> wait_lock(wait_mutex);
+				this->game_state_locked_signal.wait(wait_mutex);
+			}
+		}
+	}
+}
+
+/* static */ void VideoDriver::GameThreadThunk(VideoDriver *drv)
+{
+	drv->GameThread();
+}
+
+void VideoDriver::StartGameThread()
+{
+	if (this->is_game_threaded) {
+		this->is_game_threaded = StartNewThread(nullptr, "ottd:game", &VideoDriver::GameThreadThunk, this);
 	}
 
-	if (cur_ticks >= this->next_game_tick || (_fast_forward && !_pause_mode)) {
-		if (_fast_forward && !_pause_mode) {
-			this->next_game_tick = cur_ticks + this->GetGameInterval();
-		} else {
-			this->next_game_tick += this->GetGameInterval();
-			/* Avoid next_game_tick getting behind more and more if it cannot keep up. */
-			if (this->next_game_tick < cur_ticks - ALLOWED_DRIFT * this->GetGameInterval()) this->next_game_tick = cur_ticks;
-		}
+	DEBUG(driver, 1, "using %sthread for game-loop", this->is_game_threaded ? "" : "no ");
+}
 
-		/* The game loop is the part that can run asynchronously.
-		 * The rest except sleeping can't. */
-		this->UnlockVideoBuffer();
-		::GameLoop();
-		this->LockVideoBuffer();
+void VideoDriver::Tick()
+{
+	auto now = std::chrono::steady_clock::now();
+
+	/* If more than a millisecond has passed, increase the _realtime_tick. */
+	if (now - this->last_realtime_tick >= std::chrono::milliseconds(1)) {
+		auto delta = std::chrono::duration_cast<std::chrono::milliseconds>(now - this->last_realtime_tick);
+		_realtime_tick += delta.count();
+		this->last_realtime_tick += delta;
+	}
+
+	if (!this->is_game_threaded && now >= this->next_game_tick) {
+		this->GameLoop();
 
 		/* For things like dedicated server, don't run a separate draw-tick. */
 		if (!this->HasGUI()) {
 			::InputLoop();
-			UpdateWindows();
+			::UpdateWindows();
 			this->next_draw_tick = this->next_game_tick;
 		}
 	}
 
-	/* Prevent drawing when switching mode, as windows can be removed when they should still appear. */
-	if (this->HasGUI() && cur_ticks >= this->next_draw_tick && (_switch_mode == SM_NONE || HasModalProgress())) {
+	if (this->HasGUI() && now >= this->next_draw_tick) {
 		this->next_draw_tick += this->GetDrawInterval();
 		/* Avoid next_draw_tick getting behind more and more if it cannot keep up. */
-		if (this->next_draw_tick < cur_ticks - ALLOWED_DRIFT * this->GetDrawInterval()) this->next_draw_tick = cur_ticks;
+		if (this->next_draw_tick < now - ALLOWED_DRIFT * this->GetDrawInterval()) this->next_draw_tick = now;
 
-		while (this->PollEvent()) {}
 		this->InputLoop();
-		::InputLoop();
-		UpdateWindows();
+
+		{
+			/* Inform the game-thread that we want the game-state, get the
+			 * lock on the game-state, and inform the game-thread that we
+			 * got the lock. This avoids the game-thread to never yield.
+			 * See comment in the game-thread for more details. */
+			this->request_game_state_mutex = true;
+			std::lock_guard<std::mutex> lock(this->game_state_mutex);
+			this->game_state_locked_signal.notify_one();
+			this->request_game_state_mutex = false;
+
+			this->LockVideoBuffer();
+
+			while (this->PollEvent()) {}
+			::InputLoop();
+
+			/* Prevent drawing when switching mode, as windows can be removed when they should still appear. */
+			if (_switch_mode == SM_NONE || HasModalProgress()) {
+				::UpdateWindows();
+			}
+		}
 
 		this->CheckPaletteAnim();
 		this->Paint();
+
+		this->UnlockVideoBuffer();
 	}
 }
 
 void VideoDriver::SleepTillNextTick()
 {
-	/* If we are not in fast-forward, create some time between calls to ease up CPU usage. */
-	if (!_fast_forward || _pause_mode) {
-		/* See how much time there is till we have to process the next event, and try to hit that as close as possible. */
-		auto next_tick = std::min(this->next_draw_tick, this->next_game_tick);
-		auto now = std::chrono::steady_clock::now();
+	auto next_tick = this->next_draw_tick;
+	auto now = std::chrono::steady_clock::now();
 
-		if (next_tick > now) {
-			this->UnlockVideoBuffer();
-			std::this_thread::sleep_for(next_tick - now);
-			this->LockVideoBuffer();
-		}
+	if (!this->is_game_threaded) {
+		next_tick = min(next_tick, this->next_game_tick);
+	}
+
+	if (next_tick > now) {
+		std::this_thread::sleep_for(next_tick - now);
 	}
 }
