b251bd37f432283f413baeb89df62e31fa8b4566
Rubidium <rubidium@openttd.org>
2024-02-25 19:42:28 +0100

    Codechange: use int32_t instead of uint16_t for scroll bar position/size/capacity

--- src/widget.cpp
+++ src/widget.cpp
@@ -2266,9 +2266,9 @@ void NWidgetViewport::UpdateViewportCoordinates(Window *w)
  */
 int Scrollbar::GetScrolledRowFromWidget(int clickpos, const Window * const w, WidgetID widget, int padding, int line_height) const
 {
-	uint pos = w->GetRowFromWidget(clickpos, widget, padding, line_height);
-	if (pos != INT_MAX) pos += this->GetPosition();
-	return (pos >= this->GetCount()) ? INT_MAX : pos;
+	int32_t pos = w->GetRowFromWidget(clickpos, widget, padding, line_height);
+	if (pos != INT32_MAX) pos += this->GetPosition();
+	return (pos < 0 || pos >= this->GetCount()) ? INT32_MAX : pos;
 }
 
 /**
